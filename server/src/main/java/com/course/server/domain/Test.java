6cfec7bfb794f47c482f6d322a23e21e5781d248
lnYang1999 <13624387341@163.com>
2021-03-14 16:30:53 +0800

    1. 新建公共模块server

---
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -0,0 +1,28 @@
+package com.course.server.domain;
+
+/**
+ * @project: course
+ * @description: Test实体类
+ * @author: ysp
+ * @create: 2021/03/14
+ */
+public class Test {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
