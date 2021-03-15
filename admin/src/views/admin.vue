7f34f56cac5ff2f45881de7328960760d19d5abd
lnYang1999 <13624387341@163.com>
2021-03-15 17:15:21 +0800

    1. 使用mounted解决组件初始化样式的问题

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -975,6 +975,17 @@
 </template>
 
 <script>
-$('body').removeClass('login-layout light-login');
-$('body').attr('class', 'no-skin');
+export default {
+  name: "admin",
+  mounted: function() {
+    $("body").removeClass("login-layout light-login");
+    $("body").attr("class", "no-skin");
+    // console.log("admin");
+  },
+  methods: {
+    login () {
+      this.$router.push("/admin")
+    }
+  }
+}
 </script>
