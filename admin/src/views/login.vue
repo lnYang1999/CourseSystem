7bd9e03baffd74d22d45da029f82ce003e1adcee
lnYang1999 <13624387341@163.com>
2021-03-15 17:08:09 +0800

    1. 增加login页面登录按钮点击事件，跳转到admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,9 @@
                             <span class="lbl">记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  v-on:click="login()">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
@@ -75,5 +77,10 @@
 $('body').attr('class', 'login-layout light-login');
 export default {
   name: 'login',
+  methods: {
+    login () {
+      this.$router.push("/admin")
+    }
+  }
 }
 </script>
