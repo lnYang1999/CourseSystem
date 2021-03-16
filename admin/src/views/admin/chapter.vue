e743cabd2162e14f5d10e29f2e3a085c755e82ed
lnYang1999 <13624387341@163.com>
2021-03-16 15:32:38 +0800

    1. 增加分页组件pagination.vue 2. 大章管理页面使用分页组件，可自定义初始每页5条，最多显示8个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <p>
-      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
       </button>
     </p>
+
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
@@ -83,7 +86,9 @@
 </template>
 
 <script>
+import Pagination from "../../components/pagination";
 export default {
+  components: {Pagination},
   name: 'welcome',
   data: function () {
     return {
@@ -92,19 +97,22 @@ export default {
   },
   mounted: function() {
     let _this = this;
-    _this.list();
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
     // sidebar激活样式方法一
     // this.$parent.activeSidebar("business-chapter-sidebar");
   },
   methods: {
-    list() {
+    list(page) {
       let _this = this;
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-        page: 1,
-        size: 1
+        page: page,
+        size: _this.$refs.pagination.size,
       }).then((response)=>{
         console.log("查询大章列表结果：", response);
         _this.chapters = response.data.list;
+        _this.$refs.pagination.render(page, response.data.total);
+
       })
     }
   }
