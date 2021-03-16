374b725da892beae63630500c717e9f90d88b1a1
lnYang1999 <13624387341@163.com>
2021-03-16 13:30:29 +0800

    1. 分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -2,7 +2,11 @@ package com.course.business.controller.admin;
 
 import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +23,15 @@ import java.util.List;
 @RequestMapping("/admin/chapter")
 public class ChapterController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
+
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/list")
-    public List<ChapterDto> list() {
-        return chapterService.list();
+    public PageDto list(@RequestBody PageDto pageDto) {
+        LOG.info("pageDto: {}",pageDto);
+        chapterService.list(pageDto);
+        return pageDto;
     }
 }
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,10 +26,12 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list() {
-        PageHelper.startPage(2, 1);
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
         List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
         for (int i = 0, l = chapterList.size(); i < l; i++) {
             Chapter chapter = chapterList.get(i);
@@ -35,5 +39,5 @@ public class ChapterService {
             BeanUtils.copyProperties(chapter,chapterDto);
             chapterDtoList.add(chapterDto);
         }
-        return chapterDtoList;
+        pageDto.setList(chapterDtoList);
     }}
