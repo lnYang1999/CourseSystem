abf733724651c0663b8483ac2c332ae2363d820f
lnYang1999 <13624387341@163.com>
2021-03-18 10:24:08 +0800

    1. 生成器开发：增加生成service层的代码

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -4,12 +4,20 @@ import com.course.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerGenerator {
-    static String toPath = "generator\\src\\main\\java\\com\\course\\generator\\test\\";
+    static String toServicePath = "server\\src\\main\\java\\com\\course\\server\\service\\";
 
     public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        FreemarkerUtil.generator(toPath + "Test.java");
+        String Domain = "Section";
+        String domain = "section";
+        Map<String, Object> map = new HashMap<>();
+        map.put("Domain", Domain);
+        map.put("domain", domain);
+
+        FreemarkerUtil.initConfig("service.ftl");
+        FreemarkerUtil.generator(toServicePath + Domain + "Service.java", map);
     }
 }
