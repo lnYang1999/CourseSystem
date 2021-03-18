4219a43e457c61158c739a55b7c6250a0583b30f
lnYang1999 <13624387341@163.com>
2021-03-18 11:17:16 +0800

    1. 生成器开发：增加dto生成

--- server/src/main/java/com/course/server/dto/SectionDto.java
+++ server/src/main/java/com/course/server/dto/SectionDto.java
@@ -1,26 +1,60 @@
 package com.course.server.dto;
 
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class SectionDto {
+
+    /**
+     * ID
+     */
     private String id;
 
+    /**
+     * 标题
+     */
     private String title;
 
+    /**
+     * 课程|course.id
+     */
     private String courseId;
 
+    /**
+     * 大章|chapter.id
+     */
     private String chapterId;
 
+    /**
+     * 视频
+     */
     private String video;
 
+    /**
+     * 时长|单位秒
+     */
     private Integer time;
 
+    /**
+     * 收费|C 收费；F 免费
+     */
     private String charge;
 
+    /**
+     * 顺序
+     */
     private Integer sort;
 
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdAt;
 
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updatedAt;
 
     public String getId() {
@@ -102,6 +136,7 @@ public class SectionDto {
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
+
 
     @Override
     public String toString() {
