be297e2534431280278d05a5ce7c739b1d03afc9
lnYang1999 <13624387341@163.com>
2021-03-18 12:49:10 +0800

    1. 生成器开发：前后端模板增加字段校验

--- admin/public/static/js/validator.js
+++ admin/public/static/js/validator.js
@@ -9,6 +9,10 @@ Validator = {
   },
 
   length: function (value, text, min, max) {
+    if(Tool.isEmpty(value))
+    {
+      return true;
+    }
     if (!Tool.isLength(value, min, max)) {
       Toast.warning(text + "长度" + min + "~" + max + "位");
       return false;
