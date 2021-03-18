f322bf77dce4d6ed351a69b8451b32c865744d49
lnYang1999 <13624387341@163.com>
2021-03-18 10:10:01 +0800

    1. 制作FreemarkerUtil，简化生成器的使用 2. 新增ServerGenerator，用于生成后端代码：controller service dto

---
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -0,0 +1,15 @@
+package com.course.generator.server;
+
+import com.course.generator.util.FreemarkerUtil;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
+public class ServerGenerator {
+    static String toPath = "generator\\src\\main\\java\\com\\course\\generator\\test\\";
+
+    public static void main(String[] args) throws IOException, TemplateException {
+        FreemarkerUtil.initConfig("test.ftl");
+        FreemarkerUtil.generator(toPath + "Test.java");
+    }
+}
