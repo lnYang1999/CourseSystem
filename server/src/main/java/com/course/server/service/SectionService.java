50d3f1bcbc668a6b87e2c263493be4c2517828bb
lnYang1999 <13624387341@163.com>
2021-03-21 10:40:51 +0800

    1. 保存节时，更新课程总时长 2. 增加自定义mapper

--- server/src/main/java/com/course/server/service/SectionService.java
+++ server/src/main/java/com/course/server/service/SectionService.java
@@ -22,6 +22,9 @@ public class SectionService {
     @Resource
     private SectionMapper sectionMapper;
 
+    @Resource
+    private CourseService courseService;
+
     /**
      * 列表查询
      */
@@ -53,6 +56,7 @@ public class SectionService {
         } else {
             this.update(section);
         }
+        courseService.updateTime(sectionDto.getCourseId());
     }
 
     /**
