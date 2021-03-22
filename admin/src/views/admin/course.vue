6b6649ea1f9adc40382107f040dbd8a4bea08173
lnYang1999 <13624387341@163.com>
2021-03-22 10:18:33 +0800

    1. 保存课程时，同时保存课程分类 2. 增加课程分类关联表，一门课程可以属于多个分类

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -168,6 +168,7 @@
         COURSE_CHARGE: COURSE_CHARGE,
         COURSE_STATUS: COURSE_STATUS,
         categorys: [],
+        tree: {},
       }
     },
     mounted: function() {
@@ -231,6 +232,13 @@
         ) {
           return;
         }
+
+        let categorys = _this.tree.getCheckedNodes();
+        if (Tool.isEmpty(categorys)) {
+          Toast.warning("请选择分类！");
+          return;
+        }
+        _this.course.categorys = categorys;
 
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save', _this.course).then((response)=>{
@@ -303,7 +311,7 @@
 
         let zNodes = _this.categorys;
 
-        $.fn.zTree.init($("#tree"), setting, zNodes);
+        _this.tree = $.fn.zTree.init($("#tree"), setting, zNodes);
       }
     }
   }
