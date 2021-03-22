f29e676552830405ab3dc34a22084204aadb83d1
lnYang1999 <13624387341@163.com>
2021-03-22 11:03:52 +0800

    1. 利用setInterval完成自动保存功能 2. 增加js日期格式化方法

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -167,6 +167,11 @@
             <form class="form-horizontal">
               <div class="form-group">
                 <div class="col-lg-12">
+                  {{saveContentLabel}}
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-lg-12">
                   <div id="content"></div>
                 </div>
               </div>
@@ -202,6 +207,7 @@
         COURSE_STATUS: COURSE_STATUS,
         categorys: [],
         tree: {},
+        saveContentLabel: "",
       }
     },
     mounted: function() {
@@ -385,8 +391,11 @@
           focus: true,
           height: 300
         });
+
         // 先清空历史文本
         $("#content").summernote('code', '');
+        _this.saveContentLabel = "";
+
         Loading.show();
         _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
           Loading.hide();
@@ -397,6 +406,15 @@
             if (resp.content) {
               $("#content").summernote('code', resp.content.content);
             }
+
+            // 定时自动保存
+            let saveContentInterval = setInterval(function() {
+              _this.saveContent();
+            }, 10000);
+            // 关闭内容框时，清空自动保存任务
+            $('#course-content-modal').on('hidden.bs.modal', function (e) {
+              clearInterval(saveContentInterval);
+            })
           } else {
             Toast.warning(resp.message);
           }
@@ -416,7 +434,10 @@
           Loading.hide();
           let resp = response.data;
           if (resp.success) {
-            Toast.success("内容保存成功");
+            // Toast.success("内容保存成功");
+            // let now = Tool.dateFormat("yyyy-MM-dd hh:mm:ss");
+            let now = Tool.dateFormat("yyyy-MM-dd hh:mm:ss");
+            _this.saveContentLabel = "最后保存时间：" + now;
           } else {
             Toast.warning(resp.message);
           }
