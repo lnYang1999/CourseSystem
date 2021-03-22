81bc0c19ed1cc372167637782f83b785bbd4a27d
lnYang1999 <13624387341@163.com>
2021-03-22 09:36:50 +0800

    1. 分类列表查询与显示，左右两个表格

--- business/src/main/java/com/course/business/controller/admin/CategoryController.java
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/category")
@@ -20,6 +21,17 @@ public class CategoryController {
 
     @Resource
     private CategoryService categoryService;
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/all")
+    public ResponseDto all() {
+        ResponseDto responseDto = new ResponseDto();
+        List<CategoryDto> categoryDtoList = categoryService.all();
+        responseDto.setContent(categoryDtoList);
+        return responseDto;
+    }
 
     /**
      * 列表查询
