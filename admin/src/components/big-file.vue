3d8a3bf35d9bd65a4bd0a61593f5f18f1df3b5f7
lnYang1999 <13624387341@163.com>
2021-03-27 09:47:37 +0800

    1. 大文件断点续传与极速秒传：使用文件生成md5签名，作为文件标识

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -39,6 +39,27 @@
         let formData = new window.FormData();
         let file = _this.$refs.file.files[0];
 
+        console.log(file);
+        /*
+          name: "test.mp4"
+          lastModified: 1901173357457
+          lastModifiedDate: Tue May 27 2099 14:49:17 GMT+0800 (中国标准时间) {}
+          webkitRelativePath: ""
+          size: 37415970
+          type: "video/mp4"
+        */
+
+        // 生成文件标识，标识多次上传的是不是同一个文件
+        let key = hex_md5(file);
+        let key10 = parseInt(key, 16);
+        let key62 = Tool._10to62(key10);
+        console.log(key, key10, key62);
+        /*
+          d41d8cd98f00b204e9800998ecf8427e
+          2.8194976848941264e+38
+          6sfSqfOwzmik4A4icMYuUe
+         */
+
         // 判断文件格式
         let suffixs = _this.suffixs;
         let fileName = file.name;
@@ -58,7 +79,7 @@
 
         // 文件分片
         let shardSize = 20 * 1024 * 1024;    //以20MB为一个分片
-        let shardIndex = 2;		//分片索引
+        let shardIndex = 1;		//分片索引
         let start = shardIndex * shardSize;	//当前分片起始位置
         let end = Math.min(file.size, start + shardSize); //当前分片结束位置
         let fileShard = file.slice(start, end); //从文件中截取当前的分片数据
@@ -74,6 +95,7 @@
         formData.append('name', file.name);
         formData.append('suffix', suffix);
         formData.append('size', size);
+        formData.append('key', key62);
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
           Loading.hide();
