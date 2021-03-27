6fbfd691145cebff03ce74e5b4052338b9fc69ea
lnYang1999 <13624387341@163.com>
2021-03-27 08:53:37 +0800

    1. 大文件断点续传与极速秒传：分片相关的数据传输

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -9,77 +9,85 @@
 </template>
 
 <script>
-export default {
-  name: 'big-file',
-  props: {
-    text: {
-      default: "上传大文件"
+  export default {
+    name: 'big-file',
+    props: {
+      text: {
+        default: "上传大文件"
+      },
+      inputId: {
+        default: "file-upload"
+      },
+      suffixs: {
+        default: []
+      },
+      use: {
+        default: ""
+      },
+      afterUpload: {
+        type: Function,
+        default: null
+      },
     },
-    inputId: {
-      default: "file-upload"
+    data: function () {
+      return {
+      }
     },
-    suffixs: {
-      default: []
-    },
-    use: {
-      default: ""
-    },
-    afterUpload: {
-      type: Function,
-      default: null
-    },
-  },
-  data: function () {
-    return {
-    }
-  },
-  methods: {
-    uploadFile () {
-      let _this = this;
-      let formData = new window.FormData();
-      let file = _this.$refs.file.files[0];
+    methods: {
+      uploadFile () {
+        let _this = this;
+        let formData = new window.FormData();
+        let file = _this.$refs.file.files[0];
 
-      // 判断文件格式
-      let suffixs = _this.suffixs;
-      let fileName = file.name;
-      let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
-      let validateSuffix = false;
-      for (let i = 0; i < suffixs.length; i++) {
-        if (suffixs[i].toLowerCase() === suffix) {
-          validateSuffix = true;
-          break;
+        // 判断文件格式
+        let suffixs = _this.suffixs;
+        let fileName = file.name;
+        let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+        let validateSuffix = false;
+        for (let i = 0; i < suffixs.length; i++) {
+          if (suffixs[i].toLowerCase() === suffix) {
+            validateSuffix = true;
+            break;
+          }
         }
+        if (!validateSuffix) {
+          Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+          $("#" + _this.inputId + "-input").val("");
+          return;
+        }
+
+        // 文件分片
+        let shardSize = 20 * 1024 * 1024;    //以20MB为一个分片
+        let shardIndex = 2;		//分片索引
+        let start = shardIndex * shardSize;	//当前分片起始位置
+        let end = Math.min(file.size, start + shardSize); //当前分片结束位置
+        let fileShard = file.slice(start, end); //从文件中截取当前的分片数据
+        let size = file.size;
+        let shardTotal = Math.ceil(size / shardSize); //总片数
+
+        // key："shard"必须和后端controller参数名一致
+        formData.append('shard', fileShard);
+        formData.append('shardIndex', shardIndex);
+        formData.append('shardSize', shardSize);
+        formData.append('shardTotal', shardTotal);
+        formData.append('use', _this.use);
+        formData.append('name', file.name);
+        formData.append('suffix', suffix);
+        formData.append('size', size);
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
+          Loading.hide();
+          let resp = response.data;
+          console.log("上传文件成功：", resp);
+          _this.afterUpload(resp);
+          $("#" + _this.inputId + "-input").val("");
+        });
+      },
+
+      selectFile () {
+        let _this = this;
+        $("#" + _this.inputId + "-input").trigger("click");
       }
-      if (!validateSuffix) {
-        Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
-        $("#" + _this.inputId + "-input").val("");
-        return;
-      }
-
-      // 文件分片
-      let shardSize = 20 * 1024 * 1024;    //以20MB为一个分片
-      let shardIndex = 1;		//分片索引
-      let start = shardIndex * shardSize;	//当前分片起始位置
-      let end = Math.min(file.size, start + shardSize); //当前分片结束位置
-      let fileShard = file.slice(start, end); //从文件中截取当前的分片数据
-
-      // key："file"必须和后端controller参数名一致
-      formData.append('file', fileShard);
-      formData.append('use', _this.use);
-      Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response)=>{
-        Loading.hide();
-        let resp = response.data;
-        console.log("上传文件成功：", resp);
-        _this.afterUpload(resp);
-        $("#" + _this.inputId + "-input").val("");
-      });
-    },
-
-    selectFile () {
-      let _this = this;
-      $("#" + _this.inputId + "-input").trigger("click");
     }
   }
-}
 </script>
