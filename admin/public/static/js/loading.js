5cbb5e4d2b64b9d3739200d1f77f58249186f864
lnYang1999 <13624387341@163.com>
2021-03-28 09:22:16 +0800

    1. 大文件断点续传与极速秒传：增加上传进度条组件

--- admin/public/static/js/loading.js
+++ admin/public/static/js/loading.js
@@ -3,6 +3,7 @@ Loading = {
     $.blockUI({
       message: '<img src="/static/image/loading.gif" />',
       css: {
+        zIndex: "10011",
         padding: "10px",
         left: "50%",
         width: "80px",
@@ -14,6 +15,6 @@ Loading = {
     // 本地查询速度太快，loading显示一瞬间，故意做个延迟
     setTimeout(function () {
       $.unblockUI();
-    }, 300)
+    }, 500)
   }
 };
