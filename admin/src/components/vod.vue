0bd48f6085a281073912190235810f823c4bc940
lnYang1999 <13624387341@163.com>
2021-03-30 09:35:10 +0800

    1. 视频加密与授权播放：增加视频点播文件上传功能

--- admin/src/components/vod.vue
+++ admin/src/components/vod.vue
@@ -4,37 +4,34 @@
             v-bind:suffixs="suffixs"
             v-bind:use="use"
             v-bind:after-upload="afterUpload"
-            v-bind:shard-size="shardSize"
-            v-bind:url="'oss-append'"></big-file>
+            v-bind:shard-size="1000 * 1024 * 1024"
+            v-bind:url="'vod'"></big-file>
 </template>
 
 <script>
-import BigFile from "./big-file";
-export default {
-  components: {BigFile},
-  name: 'vod',
-  props: {
-    text: {
-      default: "上传VOD"
+  import BigFile from "./big-file";
+  export default {
+    components: {BigFile},
+    name: 'vod',
+    props: {
+      text: {
+        default: "上传VOD"
+      },
+      inputId: {
+        default: "vod-upload"
+      },
+      suffixs: {
+        default: []
+      },
+      use: {
+        default: ""
+      },
+      afterUpload: {
+        type: Function,
+        default: null
+      },
     },
-    inputId: {
-      default: "vod-upload"
-    },
-    suffixs: {
-      default: []
-    },
-    use: {
-      default: ""
-    },
-    shardSize: {
-      default: 50 * 1024
-    },
-    afterUpload: {
-      type: Function,
-      default: null
-    },
-  },
-  methods: {
+    methods: {
+    }
   }
-}
 </script>
