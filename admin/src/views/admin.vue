08acf4d645d35ded7e69375efe7f2aba7c3c4dff
lnYang1999 <13624387341@163.com>
2021-03-31 10:21:28 +0800

    1. 用户管理与登录：登录后前端保存登录信息并显示

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -282,10 +282,9 @@
               <a data-toggle="dropdown" href="#" class="dropdown-toggle">
                 <img class="nav-user-photo" src="../../public/ace/assets/images/avatars/user.jpg" alt="Jason's Photo" />
                 <span class="user-info">
-									<small>Welcome,</small>
-									Jason
-								</span>
-
+                  <small>欢迎，</small>
+                  {{loginUser.name}}
+                </span>
                 <i class="ace-icon fa fa-caret-down"></i>
               </a>
 
@@ -356,7 +355,7 @@
           <li class="" id="welcome-sidebar">
             <router-link to="/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
-              <span class="menu-text"> 欢迎 </span>
+              <span class="menu-text"> 主界面 </span>
             </router-link>
 
             <b class="arrow"></b>
@@ -512,6 +511,11 @@
 <script>
   export default {
     name: "admin",
+    data: function() {
+      return {
+        loginUser: {},
+      }
+    },
     mounted: function() {
       let _this = this;
       $("body").removeClass("login-layout light-login");
@@ -521,6 +525,8 @@
       _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
 
       $.getScript('/ace/assets/js/ace.min.js');
+
+      _this.loginUser = Tool.getLoginUser();
     },
     watch: {
       $route: {
