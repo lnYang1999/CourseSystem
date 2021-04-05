9b4cdb159e451efe197c9686eed916879bdca5ab
lnYang1999 <13624387341@163.com>
2021-04-05 16:35:42 +0800

    1. 课程列表页面开发，前端分页 2. 增加分页组件，比admin简单，使用rem

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -3,6 +3,12 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row">
+          <div class="col-md-12">
+            <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+          </div>
+        </div>
+        <br>
+        <div class="row">
           <div v-for="o in courses" class="col-md-4">
             <the-course v-bind:course="o"></the-course>
           </div>
@@ -15,41 +21,44 @@
 </template>
 
 <script>
-import TheCourse from "../components/the-course";
+  import TheCourse from "../components/the-course";
+  import Pagination from "../components/pagination";
 
-export default {
-  components: {TheCourse},
-  name: 'list',
-  data: function () {
-    return {
-      courses: [],
-    }
-  },
-  mounted() {
-    let _this = this;
-    _this.listCourse(1);
-  },
-  methods: {
-    /**
-     * 查询课程列表
-     */
-    listCourse(page) {
-      let _this = this;
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
-        page: page,
-        size: 3,
-      }).then((response) => {
-        let resp = response.data;
-        if (resp.success) {
-          _this.courses = resp.content.list;
-        }
-      }).catch((response) => {
-        console.log("error：", response);
-      })
+  export default {
+    components: {Pagination, TheCourse},
+    name: 'list',
+    data: function () {
+      return {
+        courses: [],
+      }
     },
+    mounted() {
+      let _this = this;
+      _this.$refs.pagination.size = 1;
+      _this.listCourse(1);
+    },
+    methods: {
+      /**
+       * 查询课程列表
+       */
+      listCourse(page) {
+        let _this = this;
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
+          page: page,
+          size: _this.$refs.pagination.size,
+        }).then((response) => {
+          let resp = response.data;
+          if (resp.success) {
+            _this.courses = resp.content.list;
+            _this.$refs.pagination.render(page, resp.content.total);
+          }
+        }).catch((response) => {
+          console.log("error：", response);
+        })
+      },
 
+    }
   }
-}
 </script>
 <style>
 
