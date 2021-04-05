4f3b92bc41c10af40ddfc046cf17dec0fe3f6d71
lnYang1999 <13624387341@163.com>
2021-04-05 15:44:31 +0800

    1. 网站首页开发，底部文案修改

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -2,17 +2,17 @@
   <footer class="text-muted">
     <div class="container">
       <p class="float-right">
-        <a href="#">Back to top</a>
+        <a href="#">回到顶部</a>
       </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>New to Bootstrap? <a href="https://v4.bootcss.com/">Visit the homepage</a> or read our <a href="/docs/getting-started/introduction/">getting started guide</a>.</p>
+      <p>欢迎使用在线视频课程系统</p>
+      <p>2099&nbsp;bjtu.com&nbsp;&nbsp;杨烁平 毕设项目</p>
     </div>
   </footer>
 </template>
 
 <script>
 
-export default {
-  name: 'theFooter',
-}
+  export default {
+    name: 'theFooter',
+  }
 </script>
