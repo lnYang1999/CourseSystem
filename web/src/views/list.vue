d10039d234518670dbe6eef8827828c941fc3ec7
lnYang1999 <13624387341@163.com>
2021-04-05 16:41:01 +0800

    1. 课程列表页面开发，点击二级分类时显示激活状态

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -151,6 +151,8 @@
        */
       onClickLevel2(level2Id) {
         let _this = this;
+        $("#category-" + level2Id).siblings("a").removeClass("on");
+        $("#category-" + level2Id).addClass("on");
       },
 
     }
