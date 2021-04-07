a913df674a25a253b94cc0f4a4f3aadd1bfe383f
lnYang1999 <13624387341@163.com>
2021-04-07 10:01:01 +0800

    1. 增加会员管理页面，控台的会员管理，不能新增，修改，删除。可以查看会员信息，重置密码，报表统计等操作

--- admin/src/views/admin/clubber.vue
+++ admin/src/views/admin/clubber.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        新增
-      </button>
-      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
@@ -17,86 +12,27 @@
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
-                    <th>id</th>
-            <th>手机号</th>
-            <th>密码</th>
-            <th>昵称</th>
-            <th>头像url</th>
-            <th>注册时间</th>
-        <th>操作</th>
+        <th>id</th>
+        <th>手机号</th>
+        <th>密码</th>
+        <th>昵称</th>
+        <th>头像url</th>
+        <th>注册时间</th>
       </tr>
       </thead>
 
       <tbody>
-      <tr v-for="clubber in clubbers">
-              <td>{{clubber.id}}</td>
-              <td>{{clubber.mobile}}</td>
-              <td>{{clubber.password}}</td>
-              <td>{{clubber.name}}</td>
-              <td>{{clubber.photo}}</td>
-              <td>{{clubber.registerTime}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(clubber)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(clubber.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-        </td>
+      <tr v-for="member in members">
+        <td>{{member.id}}</td>
+        <td>{{member.mobile}}</td>
+        <td>{{member.password}}</td>
+        <td>{{member.name}}</td>
+        <td>{{member.photo}}</td>
+        <td>{{member.registerTime}}</td>
       </tr>
       </tbody>
     </table>
 
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">表单</h4>
-          </div>
-          <div class="modal-body">
-            <form class="form-horizontal">
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">手机号</label>
-                      <div class="col-sm-10">
-                        <input v-model="clubber.mobile" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">密码</label>
-                      <div class="col-sm-10">
-                        <input v-model="clubber.password" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">昵称</label>
-                      <div class="col-sm-10">
-                        <input v-model="clubber.name" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">头像url</label>
-                      <div class="col-sm-10">
-                        <input v-model="clubber.photo" class="form-control">
-                      </div>
-                    </div>
-                    <div class="form-group">
-                      <label class="col-sm-2 control-label">注册时间</label>
-                      <div class="col-sm-10">
-                        <input v-model="clubber.registerTime" class="form-control">
-                      </div>
-                    </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
   </div>
 </template>
 
@@ -104,11 +40,11 @@
   import Pagination from "../../components/pagination";
   export default {
     components: {Pagination},
-    name: "business-clubber",
+    name: "business-member",
     data: function() {
       return {
-        clubber: {},
-        clubbers: [],
+        member: {},
+        members: [],
       }
     },
     mounted: function() {
@@ -116,92 +52,26 @@
       _this.$refs.pagination.size = 5;
       _this.list(1);
       // sidebar激活样式方法一
-      // this.$parent.activeSidebar("business-clubber-sidebar");
+      // this.$parent.activeSidebar("business-member-sidebar");
 
     },
     methods: {
-      /**
-       * 点击【新增】
-       */
-      add() {
-        let _this = this;
-        _this.clubber = {};
-        $("#form-modal").modal("show");
-      },
-
-      /**
-       * 点击【编辑】
-       */
-      edit(clubber) {
-        let _this = this;
-        _this.clubber = $.extend({}, clubber);
-        $("#form-modal").modal("show");
-      },
-
       /**
        * 列表查询
        */
       list(page) {
         let _this = this;
         Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/clubber/list', {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/member/list', {
           page: page,
           size: _this.$refs.pagination.size,
         }).then((response)=>{
           Loading.hide();
           let resp = response.data;
-          _this.clubbers = resp.content.list;
+          _this.members = resp.content.list;
           _this.$refs.pagination.render(page, resp.content.total);
 
         })
-      },
-
-      /**
-       * 点击【保存】
-       */
-      save() {
-        let _this = this;
-
-        // 保存校验
-        if (1 != 1
-                || !Validator.length(_this.clubber.mobile, "手机号", 1, 11)
-                || !Validator.require(_this.clubber.password, "密码")
-                || !Validator.length(_this.clubber.name, "昵称", 1, 50)
-                || !Validator.length(_this.clubber.photo, "头像url", 1, 200)
-        ) {
-          return;
-        }
-
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/clubber/save', _this.clubber).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if (resp.success) {
-            $("#form-modal").modal("hide");
-            _this.list(1);
-            Toast.success("保存成功！");
-          } else {
-            Toast.warning(resp.message)
-          }
-        })
-      },
-
-      /**
-       * 点击【删除】
-       */
-      del(id) {
-        let _this = this;
-        Confirm.show("删除会员后不可恢复，确认删除？", function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/clubber/delete/' + id).then((response)=>{
-            Loading.hide();
-            let resp = response.data;
-            if (resp.success) {
-              _this.list(1);
-              Toast.success("删除成功！");
-            }
-          })
-        });
       }
     }
   }
--- business/src/main/java/com/course/business/controller/admin/ClubberController.java
+++ business/src/main/java/com/course/business/controller/admin/ClubberController.java
@@ -1,13 +1,14 @@
 package com.course.business.controller.admin;
 
-import com.course.server.dto.ClubberDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.ClubberService;
-import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -29,33 +30,6 @@ public class ClubberController {
         ResponseDto responseDto = new ResponseDto();
         clubberService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    /**
-     * 保存，id有值时更新，无值时新增
-     */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody ClubberDto clubberDto) {
-        // 保存校验
-        ValidatorUtil.length(clubberDto.getMobile(), "手机号", 1, 11);
-        ValidatorUtil.require(clubberDto.getPassword(), "密码");
-        ValidatorUtil.length(clubberDto.getName(), "昵称", 1, 50);
-        ValidatorUtil.length(clubberDto.getPhoto(), "头像url", 1, 200);
-
-        ResponseDto responseDto = new ResponseDto();
-        clubberService.save(clubberDto);
-        responseDto.setContent(clubberDto);
-        return responseDto;
-    }
-
-    /**
-     * 删除
-     */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        clubberService.delete(id);
         return responseDto;
     }
 }
